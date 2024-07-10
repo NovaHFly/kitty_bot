fab72899fc55c1636401490b8cbeedfcff7ea402
NovaHFly <mugiviska@gmail.com>
2024-07-10 09:55:02 +0300

    feat: Add reply keyboard functionality

--- kitty_bot/main.py
+++ kitty_bot/main.py
@@ -6,9 +6,20 @@ from telebot.types import Message
 
 from kitty_bot import config
 
+CAT_API = 'https://api.thecatapi.com/v1/images/search'
+
 bot = tb.TeleBot(token=config.TOKEN)
 
-CAT_API = 'https://api.thecatapi.com/v1/images/search'
+newcat_keyboard = tb.types.ReplyKeyboardMarkup(resize_keyboard=True)
+newcat_keyboard.row(
+    tb.types.KeyboardButton('Который час?'),
+    tb.types.KeyboardButton('Определи мой ip'),
+)
+newcat_keyboard.row(
+    tb.types.KeyboardButton('/random_digit'),
+    tb.types.KeyboardButton('/newcat'),
+    tb.types.KeyboardButton('/remove_keyboard'),
+)
 
 
 def get_cat_url() -> str:
@@ -27,8 +38,17 @@ def greeting(message: Message) -> None:
     chat_id = chat.id
     name = chat.first_name
     text = f'Привет, {name}. Посмотри, какого котика я тебе нашёл'
-    bot.send_message(chat_id=chat_id, text=text)
+    bot.send_message(chat_id=chat_id, text=text, reply_markup=newcat_keyboard)
     send_cat_picture(chat_id)
+
+
+@bot.message_handler(commands=['remove_keyboard'])
+def remove_keyboard(message: Message) -> None:
+    bot.send_message(
+        chat_id=message.chat.id,
+        text='Мяу!',
+        reply_markup=tb.types.ReplyKeyboardRemove(),
+    )
 
 
 @bot.message_handler(commands=['newcat'])
