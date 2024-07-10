eb45afd2b20c4928eaa2ee95d6d74eea2ce959b3
NovaHFly <mugiviska@gmail.com>
2024-07-10 08:42:43 +0300

    feat: Add func to send cat pictures to user

---
+++ kitty_bot/config.py
@@ -0,0 +1,4 @@
+import os
+
+OWNER_CHAT_ID = int(os.environ['OWNER_CHAT_ID'])
+TOKEN = os.environ['TOKEN']
---
+++ kitty_bot/main.py
@@ -0,0 +1,53 @@
+#! .venv/scripts/python
+
+import requests as req
+import telebot as tb
+from telebot.types import Message
+
+from kitty_bot import config
+
+bot = tb.TeleBot(token=config.TOKEN)
+
+CAT_API = 'https://api.thecatapi.com/v1/images/search'
+
+
+def get_cat_url() -> str:
+    (res_json,) = req.get(CAT_API).json()
+    return res_json['url']
+
+
+def send_cat_picture(chat_id: int) -> None:
+    cat_url = get_cat_url()
+    bot.send_photo(chat_id=chat_id, photo=cat_url)
+
+
+@bot.message_handler(commands=['start'])
+def greeting(message: Message) -> None:
+    chat = message.chat
+    chat_id = chat.id
+    name = chat.first_name
+    text = f'Привет, {name}. Посмотри, какого котика я тебе нашёл'
+    bot.send_message(chat_id=chat_id, text=text)
+    send_cat_picture(chat_id)
+
+
+@bot.message_handler(commands=['newcat'])
+def new_cat(message: Message) -> None:
+    bot.send_message(chat_id=message.chat.id, text='Вам телеграмма!')
+    send_cat_picture(message.chat.id)
+
+
+@bot.message_handler(content_types=['text'])
+def echo(message: Message) -> None:
+    chat_id = message.chat.id
+    text = 'Привет, я KittyBot!'
+    bot.send_message(chat_id=chat_id, text=text)
+
+
+def main() -> None:
+    print('Bot is running!')
+    bot.polling()
+
+
+if __name__ == '__main__':
+    main()
